64a8152c92c5348ba6123746ab82e2a8c9b20475
Loreny Ielpo <lorenyielpo@gmail.com>
2019-08-31 02:42:46 -0300

    componente modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="estiloModal">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        estiloModal:{
+            required: true,
+            type: String
+        }
+    }    
+}
+</script>
+
+<style scoped>
+
+</style>

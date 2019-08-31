36e1d6b7ba3f4d367cdfd79924c019071ab9d021
Loreny Ielpo <lorenyielpo@gmail.com>
2019-08-31 02:40:41 -0300

    Componente botão

---
+++ src/components/Btn.vue
@@ -0,0 +1,44 @@
+<template>
+  <button @click="disparaAcao()" :class="estiloBtn" :type="tipo">{{ rotulo }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+      tipo: { 
+          required: true,
+          type: String
+      },
+      rotulo: {
+          required: true,
+          type: String
+      },
+      estilo: String
+    },
+  
+
+  methods: {
+    disparaAcao() {
+      this.$emit('btnAtivado');}
+  },
+
+  computed: {
+      estiloBtn(){
+          if(this.estilo == 'padrao' || '') return 'padrao';
+          if(this.estilo == 'perigo') return 'btn-pink';
+      }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  color: gray;
+  width: 10%;
+  font-size: 8px;
+}
+
+.padrao {
+  background-color: red;
+}
+</style>

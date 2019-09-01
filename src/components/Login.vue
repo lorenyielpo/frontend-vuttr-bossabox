d72d46a5b8ba1103585b99b4097cf58463c28ac2
Loreny Ielpo <lorenyielpo@gmail.com>
2019-09-01 16:31:06 -0300

    Estilo e ajuste de erros

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-    <form @submit.prevent="logar()">
+    <form @submit.prevent="signIn()" class="form">
+      <h2>Login</h2>
       <label>E-mail</label>
-      <input type="email" id="email" placeholder="E-mail" />
+      <input type="email" id="email" placeholder="E-mail" required />
 
       <label>Password</label>
-      <input type="password" id="password" placeholder="Password" />
+      <input type="password" id="password" placeholder="Password" required />
+      
+      <div v-if="show" class="error">
+      <font-awesome-icon icon="exclamation-triangle" size="lg" style="margin: auto 2%"/>
+      <p>{{error}}</p>
+      </div>
 
-      <input type="submit" value="Send" />
+      <my-button styleBtn="default" content="Login" />
     </form>
   </div>
 </template>
 
 <script>
+import MyButton from "./MyButton.vue";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 export default {
+  components: {
+    "my-button": MyButton,
+    "font-awesome-icon": FontAwesomeIcon
+  },
+  data() {
+    return {
+      error: "",
+      show: false
+    };
+  },
   methods: {
-    logar() {
+    signIn() {
       let email = document.querySelector("#email").value;
       let password = document.querySelector("#password").value;
 
@@ -25,17 +43,43 @@ export default {
       };
 
       this.$http
-        .post("http://localhost:7000/v1/login", logado)
+        .post("http://localhost:3000/v1/login", logado)
         .then(res => res.json())
-        .then(auth => {
-          localStorage.setItem("auth", auth.auth);
-          localStorage.setItem("token", `Bearer ${auth.token}`);
-          location.reload();
-        });
+        .then(
+          auth => {
+            localStorage.setItem("auth", auth.auth);
+            localStorage.setItem("token", `Bearer ${auth.token}`);
+            location.reload();
+          },
+          err => {
+            if (err.status == 500) {
+              this.error = "Oooops, something went wrong :(";
+              this.show = true;
+            }
+          }
+        );
     }
   }
 };
 </script>
 
 <style scoped>
+p {
+  text-align: center;
+  text-align: center;
+  font: "Semibold" 20px/26px "Source Sans Pro";
+  color: #ffffff;
+}
+
+.error{
+  background: #FFBB43 0% 0% no-repeat padding-box;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+}
+
+
+
 </style>

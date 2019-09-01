e0a929cd330d979feb4278c82b5a8a3276d9f082
Loreny Ielpo <lorenyielpo@gmail.com>
2019-09-01 16:30:51 -0300

    estilos do componente

--- src/components/NewUser.vue
+++ src/components/NewUser.vue
@@ -1,50 +1,58 @@
 <template>
   <div>
-    <form @submit.prevent="cadastrar()">
+    <form @submit.prevent="signUp()" class="form">
+      <h2>Sign Up</h2>
       <label>E-mail</label>
-      <input type="email" id="email" placeholder="E-mail" />
+      <input type="email" id="email" placeholder="E-mail" required />
 
       <label>Password</label>
-      <input type="password" id="password" placeholder="Password" />
+      <input type="password" id="password" placeholder="Password" required />
 
       <label>Username</label>
-      <input type="text" id="username" placeholder="Username" />
+      <input type="text" id="username" placeholder="Username" required />
 
-      <input type="submit" value="Send"/>
+      <my-button styleBtn="default" content="Sign in" />
     </form>
   </div>
 </template>
 
 <script>
+import MyButton from "./MyButton.vue";
 export default {
-    methods: {
-      cadastrar(){
-          let email = document.querySelector("#email").value;
-          let password = document.querySelector("#password").value;
-          let username = document.querySelector("#username").value;
+  components: {
+    "my-button": MyButton
+  },
+  methods: {
+    signUp() {
+      let email = document.querySelector("#email").value;
+      let password = document.querySelector("#password").value;
+      let username = document.querySelector("#username").value;
 
-          let user = {
-              email,
-              password,
-              username
-          }
+      let user = {
+        email,
+        password,
+        username
+      };
 
-          this.$http.post("http://localhost:7000/v1/user", user)
-          .then(res => res.json())
-          .then(login => {
-              let logado = {
-                  email,
-                  password
-              }
+      this.$http
+        .post("http://localhost:3000/v1/user", user)
+        .then(res => res.json())
+        .then(user => {
+          let loged = {
+            email,
+            password
+          };
 
-              this.$http.post("http://localhost:7000/v1/login", logado)
-              .then(res => res.json())
-              .then(auth => {
-                  localStorage.setItem("auth", auth.auth);
-                  localStorage.setItem("token", `Bearer ${auth.token}`);
-              }) 
-          })
-      }
+          this.$http
+            .post("http://localhost:3000/v1/login", loged)
+            .then(res => res.json())
+            .then(auth => {
+              localStorage.setItem("auth", auth.auth);
+              localStorage.setItem("token", `Bearer ${auth.token}`);
+              location.reload();
+            });
+        });
+    }
   }
 };
 </script>

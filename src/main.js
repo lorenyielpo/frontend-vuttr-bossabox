30a953fee2fd9535a0b4448f62c0dc85d38342f1
Loreny Ielpo <lorenyielpo@gmail.com>
2019-09-01 16:30:01 -0300

    Instalação do fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import * as fab from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+ 
+library.add(fab.faSearch);
+library.add(fab.faExclamationTriangle);
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 Vue.use(VueResource);
 
